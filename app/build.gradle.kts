7d59c9be3f7054aa5242f205b4de9030fe1da3f0
Jack3995 <igor3995@gmail.com>
2025-09-12 12:57:18 +0200

    Убраны/добавлены нужные/ненужные уведомления

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.jack3995.webtomarkdown"
         minSdk = 26
         targetSdk = 36
-        versionCode = 30
-        versionName = "1.9.2"
+        versionCode = 31
+        versionName = "1.9.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
